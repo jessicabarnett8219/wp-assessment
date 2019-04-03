d223ee3cc52e403fb24dc03956ab88162058d04f
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-04-02 22:18:56 -0500

    Set up new blank theme and custom homepage template; Add 5 most recent blog posts to home page

---
+++ wp-content/themes/jbtwentyone/front-page.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Custom Homepage
+ */
+
+get_header();
+?>
+<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+<ul>
+<!-- // Define our WP Query Parameters -->
+<?php $the_query = new WP_Query( 'posts_per_page=5' ); ?>
+
+<!-- // Start our WP Query -->
+<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+<!-- // Display the Post Title with Hyperlink -->
+<li>
+<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+<!-- // Display the Post Excerpt -->
+<?php the_excerpt(__('(more…)')); ?></li>
+
+<!-- // Repeat the process and reset once it hits the limit -->
+<?php
+endwhile;
+wp_reset_postdata();
+?>
+</ul>
+        </main>
+        </section>
+<!-- two custom fields -->
+<!-- lastest 5 posts previews with links -->
+
+<?php get_footer(); ?>
+
--- wp-content/themes/jbtwentyone/functions-1.php
+++ wp-content/themes/jbtwentyone/functions-1.php
@@ -8,3 +8,8 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function mytheme_post_thumbnails() {
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
